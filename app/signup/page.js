107373e2e5741d62e9b1e4c6e37373d421ff4a67
Md Emon <md.emon.au@gmail.com>
2023-12-05 08:29:01 -0800

    add reg page

---
+++ app/signup/page.js
@@ -0,0 +1,105 @@
+"use client";
+
+import ThemeButton from "@/components/common/ThemeButton";
+import Image from "next/image";
+import React, { useState } from "react";
+import axios from "axios";
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSignup = async () => {
+    try {
+      // Make a request to your signup API
+      const response = await axios.post(
+        "http://localhost:9000/api/register",
+        formData
+      );
+      // Handle the response as needed
+      console.log("Signup successful", response.data);
+    } catch (error) {
+      // Handle errors
+      console.error("Signup failed", error);
+      setError(
+        error.response ? error.response.data.message : "An error occurred"
+      );
+    }
+  };
+
+  return (
+    <div className="px-6 mb-[50px] lg:mb-[90px] mt-[30px]">
+      <div className="flex items-center py-9 xl:py-0 w-full mx-auto md:w-full mx-6 md:mx-auto md:max-w-[60%] shadow-lg border-[1px] border-accent-purple ">
+        <Image
+          src={"/RomanceDose/Dose_1.png"}
+          width={415}
+          height={500}
+          style={{ maxHeight: 700, objectFit: "cover" }}
+          alt="Login"
+          className="hidden lg:block"
+        />
+        <div className="w-full max-w-[406px] px-6 md:px-0 md:mx-[80px] flex flex-col space-y-[30px]">
+          <div className="text-30 lg:text-38 text-accent-purple work-sans-500 w-fit">
+            Signup
+          </div>
+          <input
+            className="text-14 work-sans-500 placeholder:text-accent-grey border-b-[1px] border-accent-purple focus:border-none focus:outline px-2 py-[6px] w-full max-w-[396px] transition duration-300"
+            placeholder="First Name"
+            type="text"
+            name="first_name"
+            onChange={handleChange}
+          />
+          <input
+            className="text-14 work-sans-500 placeholder:text-accent-grey border-b-[1px] border-accent-purple focus:border-none focus:outline px-2 py-[6px] w-full max-w-[396px] transition duration-300"
+            placeholder="Last Name"
+            type="text"
+            name="last_name"
+            onChange={handleChange}
+          />
+          <input
+            className="text-14 work-sans-500 placeholder:text-accent-grey border-b-[1px] border-accent-purple focus:border-none focus:outline px-2 py-[6px] w-full max-w-[396px] transition duration-300"
+            placeholder="Enter email"
+            type="email"
+            name="email"
+            onChange={handleChange}
+          />
+          <input
+            className="text-14 work-sans-500 placeholder:text-accent-grey border-b-[1px] border-accent-purple focus:border-none focus:outline px-2 py-[6px] w-full max-w-[396px] transition duration-300"
+            placeholder="Enter password"
+            type="password"
+            name="password"
+            onChange={handleChange}
+          />
+          <input
+            className="text-14 work-sans-500 placeholder:text-accent-grey border-b-[1px] border-accent-purple focus:border-none focus:outline px-2 py-[6px] w-full max-w-[396px] transition duration-300"
+            placeholder="Confirm password"
+            type="password"
+            name="confirm_password"
+            onChange={handleChange}
+          />
+          <div className="mt-[900px]">
+            <ThemeButton title={"Signup"} onClick={handleSignup} />
+          </div>
+          {error && (
+            <div className="mt-4 p-4 bg-red-500 text-white rounded-md">
+              {error}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
